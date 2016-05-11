6fbc463f149f2d3c9207e4a8c73a3373a127ceb6
Yoni Kilzi <Yoni.Kilzi@gmail.com>
2016-05-11 07:41:00 +0300

    Initial commit

---
+++ ClassNamerExtension/ClassNamerFetcher.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace ClassNamerExtension
+{
+    public class ClassNamerFetcher
+    {
+        private static readonly ClassNamerFetcher instance = new ClassNamerFetcher();
+        private const string URL = "http://www.classnamer.com/index.txt?generator=generic";
+
+        private ClassNamerFetcher() { }
+
+        public static ClassNamerFetcher GetInstance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public static string Fetch()
+        {
+            WebRequest request = WebRequest.Create(URL);
+            WebResponse response = request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+            reader.Close();
+            response.Close();
+
+            return responseFromServer.Trim();
+        }
+    }
+}
---
+++ ClassNamerExtension/ClassNamerTagCloud.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ClassNamerExtension
+{
+    class ClassNamerTagCloud
+    {
+        private static readonly ClassNamerTagCloud instance = new ClassNamerTagCloud();
+        private SortedDictionary<string, List<string>> _mappings;
+
+        private ClassNamerTagCloud()
+        {
+            _mappings = new SortedDictionary<string, List<string>>();
+        }
+
+        public static ClassNamerTagCloud GetInstance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public void Add(string className)
+        {
+            List<string> words = parse(className);
+            foreach (var word in words)
+            {
+                if (_mappings.ContainsKey(word))
+                {
+                    _mappings[word].Add(className);
+                }
+                else
+                {
+                    List<string> newList = new List<string>();
+                    newList.Add(className);
+                    _mappings.Add(word, newList);
+                }
+            }
+        }
+
+        private List<string> parse(string className)
+        {
+            List<string> wordsList = new List<string>();
+            Regex.Replace(className, @"[A-Z][a-z0-9]+", delegate (Match match) {
+                wordsList.Add(match.ToString());
+                return null;
+            });
+
+            return wordsList;
+        }
+
+        public void Clear()
+        {
+            _mappings.Clear();
+        }
+
+        public void Show()
+        {
+            Console.Clear();
+            foreach (var entry in _mappings)
+            {
+                string word = entry.Key;
+                int count = entry.Value.Count;
+                Console.WriteLine("{0}: {1}", word, count);
+            }
+        }
+
+    }
+}
